0cc5506b7c90cb07020b0781bcd1032054047f54
liang <duckflew@163.com>
2026-07-04 22:05:48 +0800

    chore: simplify login placeholder

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -7,7 +7,7 @@
         <div class="flex gap-8">
           <input
             v-model="username"
-            placeholder="输入名称，名称更容易辨识"
+            placeholder="请输入名称"
             class="flex-1"
             :disabled="loading"
             autofocus
